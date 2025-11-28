4bb7418f7b387e4a0b2aa9e900ef72c43a0563e4
Mukhammadjon <rajabovmukhammadjon@gmail.com>
2025-11-28 23:11:26 +0500

    vite.config file updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ← THIS FIXES PATHS
+  base: './',  // ← THIS FIXES PATHS
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
